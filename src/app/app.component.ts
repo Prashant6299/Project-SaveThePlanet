d4ae8a8ded7676cb71bd80395975853f93e0f8eb
Prashant Singh <psingh6299@gmail.com>
2019-11-03 12:02:45 +0530

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'saveThePlanet';
+   constructor(private router: Router){
+     
+   }
+  ngOnInit(){
+    AOS.init();
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+      window.scrollTo(0, 0)
+  });
+  }
 }
